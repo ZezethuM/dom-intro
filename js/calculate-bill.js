874ebe27eb22a96af4e96b2ed6a942244b98d868
ZezethuM <zezethu15@gmail.com>
2022-04-26 12:46:10 +0200

    building dom widgets

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,8 +1,9 @@
 //get a reference to the calculate button
-
+const calculateBtnElement = document.querySelector(".calculateBtn");
 //get a reference to the billTotal element
-
+const billTotalElement = document.querySelector(".billTotal");
 //get a reference to the billString
+const billStringField = document.querySelector(".billString");
 
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
@@ -10,4 +11,32 @@
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
+function totalPhoneBill(callsAndSms) {
+	var callsArr = callsAndSms.split(',');
+  	var countCalls = 0;
+  	var countSms = 0;
+  	var total;
+  
+  for(var i = 0; i < callsArr.length; i++) {
+  	var newArr = callsArr[i].trim();
+    if(newArr == 'call') {
+    	countCalls += 2.75;
+    }
+    else if(newArr == 'sms') {
+    	countSms += 0.65;
+    }
+  }
+  total = countCalls + countSms;
+  console.log(total)
+  return total.toFixed(2); 
+}
+
+function calculateBtnClicked(){
+
+    var billString = billStringField.value;
+    var roundedBillTotal = totalPhoneBill(billString);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+
 //link the function to a click event on the calculate button
+calculateBtnElement.addEventListener('click', calculateBtnClicked);
--- js/text-bill.js
+++ js/text-bill.js
@@ -1,11 +1,42 @@
 // get a reference to the textbox where the bill type is to be entered
-
+const textboxElem = document.querySelector('.billTypeText')
 //get a reference to the add button
-
+const textTotalAddBtn = document.querySelector('.addToBillBtn')
 //create a variable that will keep track of the total bill
+const callsTotalElem = document.querySelector(".callTotalOne");
 
+const smsTotalElem = document.querySelector(".smsTotalOne");
+
+const totalCostElem = document.querySelector(".totalOne");
 //add an event listener for when the add button is pressed
 
+var callsTotal = 0;
+var smsTotal = 0;
+function textBillTotal(){
+    // get the value entered in the billType textfield
+    var billTypeEntered = textboxElem.value.trim();
+    // update the correct total
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElem.classList.add("warning");
+    }
+}
+textTotalAddBtn.addEventListener('click', textBillTotal);
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
